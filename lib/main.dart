e4d155fcab1cab7a02b9bbbf6741b9e0fbbc6fe1
FlutLab.io <>
2021-05-20 18:56:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -87,6 +87,7 @@ class _IngresoSistemaState extends State<IngresoSistema> {
               Column(
                 children: <Widget>[
                   TextField(
+                    color: Colors.amber, //aqui...
                     decoration: InputDecoration(
                       filled: true,
                       //fillColor: Colors.amber,
