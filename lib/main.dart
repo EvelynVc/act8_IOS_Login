1a14973f58851dc071668f9d6eeaf3445bbaf96b
FlutLab.io <>
2021-05-20 19:43:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -144,7 +144,7 @@ class _IngresoSistemaState extends State<IngresoSistema> {
                   Text(
                     'Yei, Ya ingrese!',
                     textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 30, color: Colors.limeAcent[700]),
+                    style: TextStyle(fontSize: 30, color: Colors.limeAccent[700]),
                   ),
                   SizedBox(height: 30.0),
                   RaisedButton(
